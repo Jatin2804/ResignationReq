cd4fa5dda965d463ac31c903446b16c4eb816f16
Jatin Suryawanshi <suryawanshijatin07@gmail.com>
2025-02-24 14:59:02 +0530

    correcting login controller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -11,7 +11,7 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   try {
     const authResponse = await authService.login(req.body);
-    res.json({ token: authResponse.token, role: authResponse.role });
+    res.json({ token: authResponse.token, role: authResponse.role ,data:authResponse.data});
   } catch (error) {
     res.status(401).json({ error: error.message });
   }
